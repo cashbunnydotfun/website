b37c54837e9623ba6fb75e0d316dcec8706d326e
cashbunnycoot <info@cashbunny.fun>
2024-11-01 14:12:32 -0400

    add file

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+// import "./App.css";
+import { Outlet } from "react-router-dom";
+import { LanguageProvider } from "./core/LanguageProvider";
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
+import { WagmiConfig } from "wagmi";
+import { bsc, localhost } from "viem/chains";
+// import { ToastContainer } from "react-toastify";
+import { switchNetwork, watchNetwork } from "wagmi/actions";
+import "react-toastify/dist/ReactToastify.css";
+
+import React from "react";
+import ReactGA from 'react-ga';
+
+// Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  
+  const TRACKING_ID = "UA-XXXXX-X"; // OUR_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
+
+  const projectId = "daad7988f4d18448c60592b5361056e6";
+  const metadata = {
+    name: "CashBunny",
+    description: "First meme coin with real utility",
+    url: "https://web3modal.com",
+    icons: ["https://avatars.githubusercontent.com/u/37784886"],
+  };
+
+  const chains = [bsc, localhost];
+  const wagmiConfig = defaultWagmiConfig({
+    chains,
+    projectId,
+    metadata,
+  });
+
+  createWeb3Modal({ wagmiConfig, projectId, chains });
+
+  watchNetwork(async (network) => {
+    // if (network.chain?.name != "bsc") {
+    //   await switchNetwork({
+    //     chainId: 56,
+    //   });
+    // }
+    if (network.chain?.name == "bsc") {
+      await switchNetwork({
+        chainId: 1337,
+      });
+    }
+    console.log(`Network is ${network.chain?.name}`)
+  });
+
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <LanguageProvider>
+        <Header />
+        <Outlet />
+        <Footer />
+      </LanguageProvider>
+    </WagmiConfig>
+  );
+}
+
+export default App;
