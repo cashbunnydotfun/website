0912d7d193c06fc0e4a7b00f492e2e47a2e053ae
cashbunnycoot <info@cashbunny.fun>
2024-11-20 22:28:53 -0400

    cleanup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,6 @@ import { Container } from "@chakra-ui/react";
 // Components
 import Stats from "../components/Stats.tsx";
 import Hero from "../components/Hero.tsx";
-import AMM from "../components/AMM.tsx";
-import Roadmap from "../components/Roadmap.tsx";
 import Details from "../components/Details.tsx";
 
 const HomePage: React.FC = () => {
@@ -14,10 +12,7 @@ const HomePage: React.FC = () => {
       <Container>
         <Hero />
         <Stats />
-         <Details />
-        {/*<ALM /> */ }
-        {/* <Roadmap /> */}
-        {/*<CTA />  */}       
+         <Details />     
       </Container>
     </>
   );
