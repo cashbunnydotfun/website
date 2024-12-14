4c3497fcc4ac21c37d19be8789de8315c499e5aa
cashbunnycoot <info@cashbunny.fun>
2024-12-13 20:04:12 -0400

    config. file

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const presaleContractAddress = "0xFF498D1fDC498652c86D978776574647c4dd8592";
+const tokenAddress = "0x2F7c6FCE82a4845726C3744df21Dc87788112B66";
+const environment = "development";
+
+export default {
+    environment,
+    presaleContractAddress,
+    tokenAddress
+};
