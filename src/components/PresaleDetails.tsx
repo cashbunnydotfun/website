98d949d9a162cc1d05f3a79c2e90a5e1659dc492
cashbunnycoot <info@cashbunny.fun>
2024-12-14 15:51:01 -0400

    style

--- src/components/PresaleDetails.tsx
+++ src/components/PresaleDetails.tsx
@@ -2,103 +2,119 @@ import React from 'react';
 import { Box, HStack, Text, Image } from '@chakra-ui/react';
 import { commify } from '../utils';
 import bnbLogo from "../assets/images/bnb.png";
-
+import {
+    StatRoot,
+    StatLabel,
+    StatValueText,
+  } from "../components/ui/stat";
+  
 const PresaleDetails = ({ isMobile, balance, tokenPrice, contributions, contributionAmount, tokensPurchased, Logo }) => {
     return (
-        <Box bg="gray.600" border="1px solid white" p={4}>
-            <HStack spacing={4}>
-                <Box w={isMobile? "80px": "90px"}>
-                    <Text fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}>Balance</Text>
-                </Box>
-                <Box w={isMobile?"80px":"130px"}>
-                    <Text
-                        color="#fe9eb4"
-                        fontWeight="bold"
-                        fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}
-                    >
-                        {commify(Number(balance.data?.formatted).toFixed(5))}
-                    </Text>
-                </Box>
-                <Box w="30px" >
-                    <Image h={5} src={bnbLogo}  />
-                </Box>
-                <Box w="auto">
-                    <Text fontWeight="bold" fontSize={{ base: "12px", sm: "12px", md: "14px", lg: "14px" }}>
-                        {isMobile ? "" : <>&nbsp;</>}BNB
-                    </Text>
-                </Box>
-            </HStack>
+        <Box bg="gray.600" border="1px solid white" pt={1} pl={4} >
+            <StatRoot >
+                <StatLabel fontSize="md" lineHeight="5px">
+                    Summary
+                </StatLabel>
+                <Text fontSize={13}  fontStyle={"italic"}  mt={-2}>
+                    Wallet information and presale details
+                 </Text>
+            </StatRoot>
 
-            <HStack mt={3} spacing={4} >
-                <Box w={isMobile? "80px": "90px"}>
-                    <Text fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}>Token price</Text>
-                </Box>
-                <Box w={isMobile?"80px":"130px"}>
-                    <Text
-                        color="#fe9eb4"
-                        fontWeight="bold"
-                        fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}
-                    >
-                        {Number(commify(tokenPrice)).toFixed(9)}
-                    </Text>
-                </Box>
-                <Box w="30px">
-                    <Image h={5} src={bnbLogo} />
-                </Box>
-                <Box w="auto">
-                    <Text fontWeight="bold" fontSize={{ base: "12px", sm: "12px", md: "14px", lg: "14px" }}>
-                        {isMobile ? "" : <>&nbsp;</>}BNB
-                    </Text>
-                </Box>
-            </HStack>
+            <Box mt={5}>
+                <HStack spacing={4}>
+                    <Box w={isMobile? "80px": "90px"}>
+                        <Text fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}>Balance</Text>
+                    </Box>
+                    <Box w={isMobile?"80px":"130px"}>
+                        <Text
+                            color="#fe9eb4"
+                            fontWeight="bold"
+                            fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}
+                        >
+                            {commify(Number(balance.data?.formatted).toFixed(5))}
+                        </Text>
+                    </Box>
+                    <Box w="30px" >
+                        <Image h={5} src={bnbLogo}  />
+                    </Box>
+                    <Box w="auto">
+                        <Text fontWeight="bold" fontSize={{ base: "12px", sm: "12px", md: "14px", lg: "14px" }}>
+                            {isMobile ? "" : <>&nbsp;</>}BNB
+                        </Text>
+                    </Box>
+                </HStack>
 
-            <HStack mt={3} spacing={4}>
-                <Box w={isMobile? "80px": "90px"}>
-                    <Text fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}>{contributions > 0 ? "Contributed" : "Contributing"}</Text>
-                </Box>
-                <Box w={isMobile?"80px":"130px"}>
-                    <Text
-                        color="#fe9eb4"
-                        fontWeight="bold"
-                        fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}
-                      >
-                        {contributions > 0 ? contributions : contributionAmount}
-                    </Text>
-                </Box>
-                <Box w="30px">
-                    <Image h={5} src={bnbLogo}  />
-                </Box>
-                <Box w="auto">
-                    <Text fontWeight="bold" fontSize={{ base: "12px", sm: "12px", md: "14px", lg: "14px" }}>
-                        {isMobile ? "" : <>&nbsp;</>}BNB
-                    </Text>
-                </Box>
-            </HStack>            
+                <HStack mt={3} spacing={4} >
+                    <Box w={isMobile? "80px": "90px"}>
+                        <Text fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}>Token price</Text>
+                    </Box>
+                    <Box w={isMobile?"80px":"130px"}>
+                        <Text
+                            color="#fe9eb4"
+                            fontWeight="bold"
+                            fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}
+                        >
+                            {Number(commify(tokenPrice)).toFixed(9)}
+                        </Text>
+                    </Box>
+                    <Box w="30px">
+                        <Image h={5} src={bnbLogo} />
+                    </Box>
+                    <Box w="auto">
+                        <Text fontWeight="bold" fontSize={{ base: "12px", sm: "12px", md: "14px", lg: "14px" }}>
+                            {isMobile ? "" : <>&nbsp;</>}BNB
+                        </Text>
+                    </Box>
+                </HStack>
 
-            <HStack spacing={4} mt={3}>
-                <Box w={isMobile? "80px": "90px"}>
-                    <Text fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}>
-                        {contributions > 0 ? "Balance" : "You get"}
-                    </Text>
-                </Box>
-                <Box w={isMobile?"80px":"130px"}>
-                    <Text
-                        color="#fe9eb4"
-                        fontWeight="bold"
-                        fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}
-                    >
-                        {commify(tokensPurchased)}
-                    </Text>
-                </Box>
-                <Box w="60px" >
-                    <Image w={"30px"} src={Logo} />
-                </Box>
-                <Box w="auto">
-                    <Text fontWeight="bold" ml={-8} fontSize={{ base: "12px", sm: "12px", md: "14px", lg: "14px" }}>
-                    &nbsp;$BUNNY
-                    </Text>
-                </Box>
-            </HStack>
+                <HStack mt={3} spacing={4}>
+                    <Box w={isMobile? "80px": "90px"}>
+                        <Text fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}>{contributions > 0 ? "Contributed" : "Contributing"}</Text>
+                    </Box>
+                    <Box w={isMobile?"80px":"130px"}>
+                        <Text
+                            color="#fe9eb4"
+                            fontWeight="bold"
+                            fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}
+                        >
+                            {contributions > 0 ? contributions : contributionAmount}
+                        </Text>
+                    </Box>
+                    <Box w="30px">
+                        <Image h={5} src={bnbLogo}  />
+                    </Box>
+                    <Box w="auto">
+                        <Text fontWeight="bold" fontSize={{ base: "12px", sm: "12px", md: "14px", lg: "14px" }}>
+                            {isMobile ? "" : <>&nbsp;</>}BNB
+                        </Text>
+                    </Box>
+                </HStack>            
+
+                <HStack spacing={4} mt={3}>
+                    <Box w={isMobile? "80px": "90px"}>
+                        <Text fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}>
+                            {contributions > 0 ? "Balance" : "You get"}
+                        </Text>
+                    </Box>
+                    <Box w={isMobile?"80px":"130px"}>
+                        <Text
+                            color="#fe9eb4"
+                            fontWeight="bold"
+                            fontSize={{ base: "11px", sm: "11px", md: "14px", lg: "14px" }}
+                        >
+                            {commify(tokensPurchased)}
+                        </Text>
+                    </Box>
+                    <Box w="60px" >
+                        <Image w={"30px"} src={Logo} />
+                    </Box>
+                    <Box w="auto">
+                        <Text fontWeight="bold" ml={-8} fontSize={{ base: "12px", sm: "12px", md: "14px", lg: "14px" }}>
+                        &nbsp;$BUNNY
+                        </Text>
+                    </Box>
+                </HStack>                
+            </Box>
         </Box>
     );
 };
