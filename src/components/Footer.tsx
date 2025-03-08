2cea77fbbd18f106dbc449f6da8b0bb1ccee84d4
cashbunnycoot <info@cashbunny.fun>
2025-03-08 00:12:44 -0400

    add to metamask button, formatting

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,84 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, SimpleGrid, Box, HStack, Center, VStack } from '@chakra-ui/react';
+import { Button, Image, SimpleGrid, Box, HStack, Center, VStack } from '@chakra-ui/react';
 import { isMobile } from "react-device-detect";
+import { ethers } from "ethers";
+import metamaskLogo from "../assets/images/metamask.svg";
+
+const { parseUnits } = ethers.utils;
+const { Web3Provider } = ethers.providers;
 
 const footerFontSize = isMobile ? 10 : 14;
+
+const AddToMetaMaskButton = ({ contractAddress, tokenSymbol, tokenDecimals }) => {
+  const addTokenToMetaMask = async () => {
+    try {
+      // Create a provider using MetaMask's injected web3 provider
+      if (typeof window.ethereum !== 'undefined') {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const provider = new Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        // Get the contract interface and ABI (replace with your token's ABI)
+        const tokenABI = [
+          "function name() public view returns (string memory)",
+          "function symbol() public view returns (string memory)",
+          "function decimals() public view returns (uint8)",
+          "function totalSupply() public view returns (uint256)",
+          "function balanceOf(address account) public view returns (uint256)",
+          "function transfer(address recipient, uint256 amount) public returns (bool)",
+        ];
+        
+        // Create a contract instance
+        const tokenContract = new ethers.Contract(contractAddress, tokenABI, signer);
+
+        // Get the token details
+        const name = await tokenContract.name();
+        const symbol = await tokenContract.symbol();
+        const decimals = await tokenContract.decimals();
+
+        // Prepare the token information for MetaMask
+        const formattedSymbol = tokenSymbol || symbol;
+        const formattedDecimals = tokenDecimals || decimals;
+
+        const hexValue = parseUnits('1', formattedDecimals);
+
+        // Add the token to MetaMask
+        await window.ethereum.request({
+          method: 'wallet_watchAsset',
+          params: {
+            type: 'ERC20',
+            options: {
+              address: contractAddress,
+              symbol: formattedSymbol,
+              decimals: formattedDecimals,
+              image: `https://example.com/token-logo.png`, // Replace with your token logo URL
+            },
+          },
+        });
+      } else {
+        console.error("MetaMask is not installed.");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Button onClick={addTokenToMetaMask} 
+      variant={"outline"}
+      fontSize={isMobile ? 6 : 10}
+      h={5}
+      mt={-5}
+      ml={2}
+      borderRadius={10}
+    >
+      Add to MetaMask
+      <Image src={metamaskLogo} w={15} ml={2} />
+    </Button>
+  );
+};
+
 
 const Footer: React.FC = () => {
   const [hasCopied, setHasCopied] = useState(false);
@@ -16,6 +91,7 @@ const Footer: React.FC = () => {
     });
   };
 
+  const address = String("0x2F7c6FCE82a4845726C3744df21Dc87788112B66");
   return (
     <footer>
       <Box mt={250}>
@@ -47,7 +123,7 @@ const Footer: React.FC = () => {
                     style={{fontSize: footerFontSize}}
                     >
                       &nbsp;&nbsp;
-                      <label>0x2F7c6FCE82a4845726C3744df21Dc87788112B66 </label>
+                      <label>{address.slice(0, 6)}...${address?.slice(-6)} </label>
                     </a> 
                     {isMobile ? <br /> : <></> }
                     &nbsp;(Binance Smart Chain)
@@ -71,10 +147,15 @@ const Footer: React.FC = () => {
                 >
                   {hasCopied ? "Copied!" : "Copy"}
                 </Button>
-            </Box>
+              </Box>
+              <Box>
+              <AddToMetaMaskButton contractAddress={"0x2F7c6FCE82a4845726C3744df21Dc87788112B66"} />
+              </Box>
                 </HStack>
                 </VStack>
+              <Box>
 
+              </Box>
 
               </Center>
               <div className="social-icons d-flex justify-content-center my-4">
